d2e51a544c9d93459446eaeed286ec316eb638ff
diego_pacini <109973383+diegopacini@users.noreply.github.com>
2023-11-29 16:57:26 +0100

    Update FFT_block.v

--- FFT_block.v
+++ FFT_block.v
@@ -29,38 +29,55 @@ input wire start_i;
 wire memsel;
 wire [9:0]address_a;
 wire [9:0]address_b;
+wire [31:0]a_real_ram1;
+wire [31:0]a_img_ram1;
+wire [31:0]b_real_ram1;
+wire [31:0]b_img_ram1;
+wire [31:0]a_real_ram2;
+wire [31:0]a_img_ram2;
+wire [31:0]b_real_ram2;
+wire [31:0]b_img_ram2;
+wire [31:0]a_real;
+wire [31:0]a_img;
+wire [31:0]b_real;
+wire [31:0]b_img;
+wire [31:0]anext_real;
+wire [31:0]anext_img;
+wire [31:0]bnext_real;
+wire [31:0]bnext_img;
 
 // Private regs
 reg i2s_get = 0;
 reg [DAC_BITS-1:0] data_R = 0;
 
 // Private assignments
-assign swap_buffers   = ;
+assign a_real = memsel ? a_real_ram2 : a_real_ram1;
+assign a_img = memsel ? a_img_ram2 : a_img_ram1;
+assign b_real = memsel ? b_real_ram2 : b_real_ram1;
+assign b_img = memsel ? b_img_ram2 : b_img_ram1;
 
 // Private instances
 addr_gen_unit fft_agu(
     .clk(clk),
     .rst_n(rst_n),
-    .address_a_o(),
-    .address_b_o(),
+    .address_a_o(address_a),
+    .address_b_o(address_b),
 	 .memsel_o(memsel),
 );
 
 butterfly_unit bfu(
-    .clk(clk), 
-    .rst_n(rst_n),
-    
-    // Audio codec physical pins
-    .codec_aud_xck_o(codec_aud_xck_o),
-    .codec_aud_bclk_o(codec_aud_bclk_o),
-    .codec_aud_adcdat_i(codec_aud_adcdat_i),
-    .codec_aud_adclrck_o(codec_aud_daclrck_o),
-    
-    // Control signals
-    .i2s_sample_data_L_i(data_L),
-    .i2s_sample_data_R_i(data_R),
-    .i2s_get_i(i2s_get),
-    .i2s_done_o(i2s_done)
+    .clk(clk), //serve?
+	 //input
+	 .twiddle, 
+	 .ina_real(a_real), 
+	 .ina_img(a_img), 
+	 .inb_real(b_real), 
+	 .inb_img(b_img), 
+	 //output
+	 .outa_real(anext_real), 
+	 .outa_img(anext_img), 
+	 .outb_real(bnext_real), 
+	 .outb_img(bnext_img)
 );
 
 twiddle_fact twiddle(
@@ -71,25 +88,33 @@ twiddle_fact twiddle(
 );
 
 cmplx_ram2port ram1(
-	address_a(address_a),
-	address_b(address_b),
-	clock(clk),
-	data_a,
-	data_b,
-	wren(memsel),
-	q_a,
-	q_b;
+	.address_a(address_a),
+	.address_b(address_b),
+	.clock(clk),
+	.data_a_real(anext_real),
+	.data_a_img(anext_img),
+	.data_b_real(bnext_real),
+	.data_b_img(bnext_img),
+	.wren(~memsel),
+	.qreal_a(a_real_ram1),
+	.qimg_a(a_img_ram1),
+	.qreal_b(b_real_ram1),
+	.qimg_b(b_img_ram1)
 );
 
 cmplx_ram2port ram2(
-	address_a(address_a),
-	address_b(address_b),
-	clock(clk),
-	data_a,
-	data_b,
-	wren(memsel),
-	q_a,
-	q_b;
+	.address_a(address_a),
+	.address_b(address_b),
+	.clock(clk),
+	.data_a_real(anext_real),
+	.data_a_img(anext_img),
+	.data_b_real(bnext_real),
+	.data_b_img(bnext_img),
+	.wren(memsel),
+	.qreal_a(a_real_ram2),
+	.qimg_a(a_img_ram2),
+	.qreal_b(b_real_ram2),
+	.qimg_b(b_img_ram2)
 );
 
 // Main FSM

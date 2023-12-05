968555be7d39bab1ca2defb9b91e5f9f93c06787
diego_pacini <109973383+diegopacini@users.noreply.github.com>
2023-12-05 14:32:58 +0100

    Update addr_gen_unit.v

--- addr_gen_unit.v
+++ addr_gen_unit.v
@@ -4,32 +4,86 @@ module addr_gen_unit(
     address_a_o,
     address_b_o,
 	 memsel_o,
-	 twiddle_addr
+	 twiddle_addr,
+	 start_i
 );
 
 //Parameters
-
+localparam s0 = 1'b0, ADDRESS_GENERATION = 1'b1;
 
 //Ports definition
 input clk;
 input rst_n;
-output memsel_o;
-output [8:0]twiddle_addr;
-output [9:0]address_a_o;
-output [9:0]address_b_o;
+input start_i;
+output reg memsel_o;
+output reg [8:0]twiddle_addr = 9'b0;
+output reg [9:0]address_a_o;
+output reg [9:0]address_b_o;
 
 //Private wires
 
 //Private regs
 
+reg sreg = s0;
+reg snext;
+reg [8:0] j = 9'b0;
+reg [3:0] i = 4'b0;
+reg [9:0] j_double = 10'b0;
+
 //Private assignments
 
-//Main FSM
+assign j_double[9:0] = {0, j[8:0]};
+
+//Aggiornamento stato FSM e contatori
 always @(posedge clk) begin
-	if(!rst_n) begin
-			fsm_state <= 0;
-   end else //case(fsm_state)
-			fsm_state <= 1;
+	if(!rst_n)
+	begin
+			sreg <= s0;
+			i <= 4'b0;
+			j <= 9'b0;
+	end
+   else
+	begin
+			sreg <= snext;
+			j <= j+1'b1;
+			if(j == 9'd511)
+				i <= i + 1'b1;
+			else
+				i <= i;
+	end
+			
 end
+
+
+
+
+// stato futuro e uscite
+always @ (sreg or j or i or start_i)
+begin
+	case(sreg)
+	s0 :
+		if(start_i) snext = ADDRESS_GENERATION;
+		else snext = s0;
+	ADDRESS_GENERATION : 
+	begin
+		if(i == 4'd9 && j == 9'd511)
+			snext = s0;
+		else
+		begin
+			snext = CALC_FFT;
+			address_a_o[9:0] = {(j_double << 1)[9-i:0], (j_double << 1)[9:9-i+1]};
+			address_b_o[9:0] = {((j_double << 1) + 1'b1)[9-i:0], ((j_double << 1)+1'b1)[9:9-i+1]};
+			twiddle_addr[8:0] += (1 << 9-i);
+			memsel_o = i[0]; 
+		end
+	end
+	default: 
+		snext = s0;
+	endcase 
+end
+
+
+
+
 
 endmodule

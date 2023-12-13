a87ab1b3e1d28de46213bee9972e52057c01949d
diego_pacini <109973383+diegopacini@users.noreply.github.com>
2023-12-13 12:43:53 +0100

    Update FFT_block.v

--- FFT_block.v
+++ FFT_block.v
@@ -7,28 +7,35 @@ module FFT_block(
     
     // control signals
     start_i,
-    end_o,
+    //end_o, per ora non serve
 );
 
 // Parameters
-localparam SAMPLE_WIDTH = 16;
-localparam SAMPLE_NUM	= 1024;
-localparam FFT_DEPTH    = $clog2(SAMPLE_NUM);
+//localparam SAMPLE_WIDTH = 16;
+//localparam SAMPLE_NUM	= 1024;
+//localparam FFT_DEPTH    = $clog2(SAMPLE_NUM);
+localparam s0 = 3'b000, CALC_FFT = 3'b10;
 
 // Ports definition
 input clk;
 input rst_n;
 
-input wire data_i;
-output wire address_o;
+input [31:0] data_i;
+output [9:0] address_o;
 
-output wire end_o;
-input wire start_i;
+//output end_o;
+input start_i;
 
 // Private wires
 wire memsel;
 wire [9:0]address_a;
 wire [9:0]address_b;
+wire [9:0]pipe_address_a;
+wire [9:0]pipe_address_b;
+wire [9:0]mem1_address_a;
+wire [9:0]mem1_address_b;
+wire [9:0]mem2_address_a;
+wire [9:0]mem2_address_b;
 wire [31:0]a_real_ram1;
 wire [31:0]a_img_ram1;
 wire [31:0]b_real_ram1;
@@ -41,6 +48,10 @@ wire [31:0]a_real;
 wire [31:0]a_img;
 wire [31:0]b_real;
 wire [31:0]b_img;
+wire [31:0]anext_real_bfu;
+wire [31:0]anext_img_bfu;
+wire [31:0]bnext_real_bfu;
+wire [31:0]bnext_img_bfu;
 wire [31:0]anext_real;
 wire [31:0]anext_img;
 wire [31:0]bnext_real;
@@ -48,16 +59,30 @@ wire [31:0]bnext_img;
 wire [31:0]tw_real;
 wire [31:0]tw_img;
 wire [8:0]tw_addr;
+wire loading;
 
 // Private regs
-reg i2s_get = 0;
-reg [DAC_BITS-1:0] data_R = 0;
+//reg i2s_get = 0;
+//reg [DAC_BITS-1:0] data_R = 0;
+reg [2:0]sreg = CALC_FFT;
+reg [2:0]snext;
+reg [8:0]j = 9'b0; //le coppie sono metà dei campioni, quindi bastano 9 bit
+reg [3:0]i = 3'b0;
+
 
 // Private assignments
 assign a_real = memsel ? a_real_ram2 : a_real_ram1;
 assign a_img = memsel ? a_img_ram2 : a_img_ram1;
 assign b_real = memsel ? b_real_ram2 : b_real_ram1;
 assign b_img = memsel ? b_img_ram2 : b_img_ram1;
+assign mem1_address_a = memsel ? pipe_address_a : address_a;
+assign mem1_address_b = memsel ? pipe_address_b : address_b;
+assign mem2_address_a = memsel ? address_a : pipe_address_a;
+assign mem2_address_b = memsel ? address_b : pipe_address_b;
+assign anext_real = loading ? data_i : anext_real_bfu;
+assign anext_img = loading ? 0 : anext_img_bfu;
+assign bnext_real = loading ? data_i : bnext_real_bfu;
+assign bnext_img = loading ? 0 : bnext_img_bfu;
 
 // Private instances
 addr_gen_unit fft_agu(
@@ -66,7 +91,10 @@ addr_gen_unit fft_agu(
     .address_a_o(address_a),
     .address_b_o(address_b),
 	 .memsel_o(memsel),
-	 .twiddle_addr(tw_addr)
+	 .twiddle_addr_o(tw_addr),
+	 .start_i(start_i), //da aggiustare
+	 .read_address_buffer_o(address_o),
+	 .loading_o(loading)
 );
 
 butterfly_unit bfu(
@@ -79,10 +107,10 @@ butterfly_unit bfu(
 	 .inb_real(b_real), 
 	 .inb_img(b_img), 
 	 //output
-	 .outa_real(anext_real), 
-	 .outa_img(anext_img), 
-	 .outb_real(bnext_real), 
-	 .outb_img(bnext_img)
+	 .outa_real(anext_real_bfu), 
+	 .outa_img(anext_img_bfu), 
+	 .outb_real(bnext_real_bfu), 
+	 .outb_img(bnext_img_bfu)
 );
 
 twiddle_fact twiddle(
@@ -94,8 +122,8 @@ twiddle_fact twiddle(
 );
 
 cmplx_ram2port ram1(
-	.address_a(address_a),
-	.address_b(address_b),
+	.address_a(mem1_address_a),
+	.address_b(mem1_address_b),
 	.clock(clk),
 	.dreal_a(anext_real),
 	.dimg_a(anext_img),
@@ -109,8 +137,8 @@ cmplx_ram2port ram1(
 );
 
 cmplx_ram2port ram2(
-	.address_a(address_a),
-	.address_b(address_b),
+	.address_a(mem2_address_a),
+	.address_b(mem2_address_b),
 	.clock(clk),
 	.dreal_a(anext_real),
 	.dimg_a(anext_img),
@@ -123,68 +151,17 @@ cmplx_ram2port ram2(
 	.qimg_b(b_img_ram2)
 );
 
-// Main FSM
-always @ (posedge clk) begin
-    if(!rst_n) begin
-        codec_buff_sel_o <= 0;
-        codec_buff_empty_o <= 0;
-        codec_buff_addr_o <= 0;
-        i2s_get <= 0;
-        sample_cnt <= 0;
-        fsm_state <= FSM_WAIT_BUFFER_0;
-    end else case(fsm_state)
-	 //FORSE I 2 BUFFER VANNO SCAMBIATI DI RUOLO
-        FSM_WAIT_BUFFER_0: begin
-            codec_buff_empty_o <= !codec_buffer_empty_ack_i;
-            fsm_state <= (codec_buff_empty_ack_i) ? FSM_WAIT_BUFFER_1 : fsm_state;
-        end
-        FSM_WAIT_BUFFER_1: begin
-            if(i2s_done)
-                i2s_get <= 0;
-            if(codec_buff_filled_i) begin
-                codec_buff_sel_o <= codec_buff_sel_o ^ 1;
-                codec_buff_empty_o <= 1'b1;
-                codec_buff_addr_o <= 0; 
-                sample_cnt <= 0;
-                wait_states <= 0;
-                fsm_state <= FSM_CONSUMING_BUFFER;
-            end
-        end
-        FSM_CONSUMING_BUFFER: begin
-            if(wait_states == RAM_READ_WAIT_STATES) begin
-                codec_buff_empty_o <= !codec_buff_empty_ack_i & (swap_buffs | codec_buff_empty_o);
-                codec_buff_sel_o   <= codec_buff_sel_o ^ swap_buffs;
-              
-                codec_buff_addr_o <= codec_buff_addr_o + 1'b1;
-                sample_cnt          <= (sample_cnt < sample_cnt_top) ? sample_cnt + 1'b1 : 0;
-                wait_states <= 0;
-                
-                i2s_get <= i2s_get | (sample_cnt == sample_cnt_top);
-                
-                case(sample_cnt)
-                    0: begin data_L[ 7:0] <= codec_buffer_data_i; data_R[ 7:0] <= codec_buffer_data_i; end
-                    1: begin data_L[15:8] <= codec_buffer_data_i; data_R[15:8] <= codec_buffer_data_i; end
-                    2: data_R[ 7:0] <= codec_buffer_data_i;
-                    3: data_R[15:8] <= codec_buffer_data_i;
-                endcase
-                
-                if(sample_cnt == sample_cnt_top) begin                    
-                    if(buffer_is_over && !codec_buffer_filled_i)
-                        fsm_state <= FSM_WAIT_BUFFER_1;
-                    else
-                        fsm_state <= (!i2s_done) ? FSM_WAIT_I2S : fsm_state;
-                end
-            end else begin
-                wait_states <= wait_states + 1'b1;
-            end
-        end
-        FSM_WAIT_I2S: begin
-            if(codec_buff_empty_ack_i)
-                codec_buff_empty_o <= 0;
+pipe2 add_a_pipeline(
+	.in(address_a), 
+	.out(pipe_address_a), 
+	.ck(clk)
+);
 
-            fsm_state <= (i2s_done && !codec_pause_i) ? FSM_CONSUMING_BUFFER : fsm_state;
-        end
-    endcase
-end
+pipe2 add_b_pipeline(
+	.in(address_b), 
+	.out(pipe_address_b), 
+	.ck(clk)
+);
+
 
 endmodule
